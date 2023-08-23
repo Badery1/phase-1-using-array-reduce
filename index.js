547043910da935d809693b76eabf634f87fc5455
barit <mmyers.com@Gmail.com>
2023-08-23 13:39:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function getTotalBatteries(batteryBatches) {
+    let totalBatteries = 0;
+    for (const batch of batteryBatches) {
+        totalBatteries += batch;
+    }
+    return totalBatteries;
+}
+const totalBatteries = getTotalBatteries(batteryBatches);
